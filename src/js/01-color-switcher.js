11a5988d9ee57807aaa6114bd885f3565b90b4e5
BeataHryniewicka <118068394+BeataHryniewicka@users.noreply.github.com>
2023-04-17 21:30:37 +0200

    .

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+let timerId = null;
+
+function getStarted() {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  btnStart.disabled = true;
+}
+
+function getStoped() {
+  clearInterval(timerId);
+  btnStart.disabled = false;
+}
+
+btnStart.addEventListener('click', getStarted);
+btnStop.addEventListener('click', getStoped);
